73cb69bf0a905cca2c50bc13d0fb4a97ba80876f
yeonnyzzang <alsrltmddus@naver.com>
2021-07-13 13:33:53 +0900

    pay service 구현중

---
+++ src/main/java/com/kh/khblind/pay/service/KakaoPayService.java
@@ -0,0 +1,6 @@
+package com.kh.khblind.pay.service;
+
+// 많은 결제 시스템 중 kakaoPay를 구현
+public class KakaoPayService implements PayService{
+
+}
---
+++ src/main/java/com/kh/khblind/pay/service/PayService.java
@@ -0,0 +1,5 @@
+package com.kh.khblind.pay.service;
+
+public interface PayService {
+	
+}
