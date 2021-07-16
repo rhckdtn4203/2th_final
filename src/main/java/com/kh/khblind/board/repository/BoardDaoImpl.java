9e69fcb21c3af3d959663b57338d8a9ab21fe784
qudcj <qudcj@LAPTOP-7I7VAH4C>
2021-07-16 18:25:00 +0900

    작성, 삭제, 수정 기능 구현

--- src/main/java/com/kh/khblind/board/repository/BoardDaoImpl.java
+++ src/main/java/com/kh/khblind/board/repository/BoardDaoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.khblind.board.entity.BoardDto;
+import com.kh.khblind.board.entity.BoardMemberVO;
 
 @Repository
 public class BoardDaoImpl implements BoardDao {
@@ -23,5 +24,37 @@ public class BoardDaoImpl implements BoardDao {
 		sqlSession.insert("board.insert", boardDto);
 		
 	}
+
+	@Override
+	public BoardDto getBoardDetail(int boardNo) {
+		
+		BoardDto boardDto = sqlSession.selectOne("board.getBoardDetail", boardNo);
+		System.out.println(boardDto);
+		
+		return boardDto;
+		
+		
+	}
+
+	@Override
+	public BoardMemberVO find(int boardNo) {
+		
+		BoardMemberVO boardMemberVO = sqlSession.selectOne("board.find", boardNo);
+	
+		return boardMemberVO;
+	}
+
+	@Override
+	public boolean edit(BoardDto boardDto) {
+		int count = sqlSession.update("board.edit", boardDto); 
+		return count > 0;
+	}
+
+	@Override
+	public boolean delete(int boardNo) {
+		int count = sqlSession.delete("board.delete", boardNo);
+		return count > 0;
+	}
+	
 
 }
