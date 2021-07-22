23c99bf65e0a0989bddb06f242ad8b752a49df5a
kc-harold <kklastchnpd@naver.com>
2021-07-22 17:25:38 +0900

    이미지 업로드 테스트

---
+++ src/test/java/com/kh/khblind/board/vote/test/Test07_bW.java
@@ -0,0 +1,40 @@
+package com.kh.khblind.board.vote.test;
+
+import org.junit.Test;
+
+import com.kh.khblind.board.entity.BoardDto;
+
+public class Test07_bW {
+
+		@Test
+		public void test1() {
+			//사용자에게 받는 정보들은
+			//BOARD_NO, MEMBER_NO, BOARD_TITLE, BOARD_CONTENT, CATEGORY_NO
+			//가 BoardVo안에 담겨서 왔다.
+			//1번 작업 - boardTable에 insert
+//			BoardDto boardDto = new BoardDto();
+//			
+//				int boardNo = 시퀀스 불러오는 Dao기능;
+//				
+//				boardDto.setBoardNo(boardNo);
+//				boardDto.setBoardTitle(boardVo.getBoardTile);
+//				boardDto.setBoardContent(boardVo.getBoardContent);
+//				
+//			
+//			sqlSession.insert("board.insertBoardTable", boardDto);
+//			
+//			//2번작업 - boardCategoryGroup 테이블에 insert
+//			
+//			BoardCategoryGroupDto boardCategoryGroupDto =  new BoardCategoryGroupDto ();
+//			
+//			boardCategoryGroupDto.setBoardCategoryNo(BoardVo.CategoryNo())
+//			boardCategoryGroupDto.setBoardNo(boardNo);
+//			
+//			sqlSession.insert("board.insertBoardCategoryGroupTable", boardCategoryGroupDto);
+//			
+//			
+//			//3번 작업 사진 
+			
+			
+		}
+}
