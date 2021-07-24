2c4a7c70fb56378b515a529560459acf70e35173
kc-harold <kklastchnpd@naver.com>
2021-07-24 18:29:25 +0900

    글 수정 수정

---
+++ src/main/java/com/kh/khblind/board/entity/BoardEditGetInfoVO.java
@@ -0,0 +1,21 @@
+package com.kh.khblind.board.entity;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class BoardEditGetInfoVO {
+	private int boardNo;
+	private int memberNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardCount;
+	private Date boardDate;
+	
+	private String jobCategoryName;
+	private String companyName;
+}
