8628dee8146287f55f750205a51ab38dbbe12bc2
kc-harold <kklastchnpd@naver.com>
2021-07-29 20:05:25 +0900

    댓글 구현(수정,삭제 필요)

--- src/main/java/com/kh/khblind/board/repository/CommentDao.java
+++ src/main/java/com/kh/khblind/board/repository/CommentDao.java
@@ -3,6 +3,9 @@ package com.kh.khblind.board.repository;
 import com.kh.khblind.board.entity.CommentsVO;
 
 public interface CommentDao {
-	void commentInsert(CommentsVO commentsVO);
-	void nestedCommentInsert(CommentsVO commentsVO);
+   void commentInsert(CommentsVO commentsVO);
+   void nestedCommentInsert(CommentsVO commentsVO);
+   boolean delete(int commentsNo);
+   boolean edit(CommentsVO commentsVO);
+   int getCommentsSequence();
 }
