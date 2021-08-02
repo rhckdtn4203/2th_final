7ede7f493e9aa3984c92c2aed8dccfab1a5a7230
rhckdtn4203 <mrk100409@gmail.com>
2021-08-02 19:17:39 +0900

    북마크 기능

--- src/main/java/com/kh/khblind/board/repository/BookmarkDao.java
+++ src/main/java/com/kh/khblind/board/repository/BookmarkDao.java
@@ -1,9 +1,13 @@
 package com.kh.khblind.board.repository;
 
+import java.util.List;
+
 import com.kh.khblind.board.entity.BookmarkDto;
+import com.kh.khblind.board.entity.BookmarkVO;
 
 public interface BookmarkDao {
 	void BookmarkInsert(BookmarkDto bookmarkDto);
 	void BookmarkDelete(BookmarkDto bookmarkDto);
 	boolean bookmarkExist(BookmarkDto bookmarkDto);
+	List<BookmarkVO> MyBookmark(int memberNo);
 }
